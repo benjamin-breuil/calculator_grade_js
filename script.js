2267b243caf2f0489bbc4d6f571ac9cdc3c4ad32
benjaminbreuil <benjamin.breuil@jobtrek.ch>
2022-11-15 13:04:59 +0100

    on click on a row changing color

--- script.js
+++ script.js
@@ -35,9 +35,9 @@ addRow(0), addRow(1), addRow(2), addRow(3), addRow(4)
 // EDIT ROW
 editRow(0), editRow(1), editRow(2), editRow(3), editRow(4)
 // REM. ROW
-removeRow(0), removeRow(1), removeRow(2), removeRow(3),removeRow(4)
+removeRow(0), removeRow(1), removeRow(2), removeRow(3), removeRow(4)
 
-function addNoteInObject(id){
+function addNoteInObject(id) {
     let note = document.getElementById("notes" + id).valueAsNumber
 
     stockNote.branche[id].notes.push(note)
@@ -45,132 +45,114 @@ function addNoteInObject(id){
     console.log(stockNote)
 }
 
-function addRow(id){
+function addRow(id) {
     const button = document.getElementById('button' + id)
 
-    button.addEventListener('click', function(){
-    let table = document.getElementById("table" + id),
-        newRow = table.insertRow(table.length),
-        cell1 = newRow.insertCell(0),
-        cell2 = newRow.insertCell(1),
-        notes = document.getElementById("notes" + id).valueAsNumber
+    button.addEventListener('click', function () {
+        let table = document.getElementById("table" + id),
+            newRow = table.insertRow(table.length),
+            cell1 = newRow.insertCell(0),
+            cell2 = newRow.insertCell(1),
+            notes = document.getElementById("notes" + id).valueAsNumber
         modules = document.getElementById("modules" + id).value
         cell1.innerHTML = modules
-        cell2.innerHTML = notes    
+        cell2.innerHTML = notes
 
-    randomIndexFunction(table, id)
-    addNoteInObject(id)
-    moyennes()
-    // Moyenne branches
-})
+        randomIndexFunction(table, id)
+        addNoteInObject(id)
+        moyennes()
+        // Moyenne branches
+    })
 }
 
-function editRow(id){
+function editRow(id) {
     const button = document.getElementById('buttonEdit' + id)
-    button.addEventListener('click', function(){
-    let table = document.getElementById("table" + id),
-        modules = document.getElementById("modules" + id).value,
-        notes = document.getElementById("notes" + id). valueAsNumber
-    randomIndexFunction(table, id)
-    
-    stockNote.branche[id].notes[rIndex - 1] = notes
-    
+    button.addEventListener('click', function () {
+        let table = document.getElementById("table" + id),
+            modules = document.getElementById("modules" + id).value,
+            notes = document.getElementById("notes" + id).valueAsNumber
+        randomIndexFunction(table, id)
 
-    table.rows[rIndex].cells[0].innerHTML = modules;
-    table.rows[rIndex].cells[1].innerHTML = notes;
-    moyennes()
+        stockNote.branche[id].notes[rIndex - 1] = notes
 
-    // Moyenne branches
-})
+
+        table.rows[rIndex].cells[0].innerHTML = modules;
+        table.rows[rIndex].cells[1].innerHTML = notes;
+        moyennes()
+
+        // Moyenne branches
+    })
 }
 
-function removeRow(id){
+function removeRow(id) {
     const button = document.getElementById('buttonREM' + id)
-    button.addEventListener('click', function(){
-    let table = document.getElementById("table" + id)
+    button.addEventListener('click', function () {
+        let table = document.getElementById("table" + id)
 
-    randomIndexFunction(table, id)
-    
-    stockNote.branche[id].notes.splice(rIndex - 1,1)
+        randomIndexFunction(table, id)
 
-    table.deleteRow(rIndex)
-    document.getElementById("notes" + id).value = "";
-    document.getElementById("modules" + id).value = "";
-    moyennes()
-})
+        stockNote.branche[id].notes.splice(rIndex - 1, 1)
+
+        table.deleteRow(rIndex)
+        document.getElementById("notes" + id).value = "";
+        document.getElementById("modules" + id).value = "";
+        moyennes()
+    })
 
 }
 
-function randomIndexFunction(tables, ids){
-    for(var i = 0; i < tables.rows.length; i++){
-        tables.rows[i].onclick = function()
-        {
+function randomIndexFunction(tables, ids) {
+    for (var i = 1; i < tables.rows.length; i++) {
+        tables.rows[i].onclick = function () {
             rIndex = this.rowIndex
             document.getElementById("modules" + ids).value = this.cells[0].innerHTML
             document.getElementById("notes" + ids).value = this.cells[1].innerHTML
+            let currentRow = rIndex
+            console.log("curreeent row " + rIndex)
             console.log(rIndex)
+            Array.from(this.parentElement.children).forEach(function(el){
+                el.classList.remove('selected-row')
+            })
+            this.classList.add('selected-row')
         }
     }
 
     return rIndex
 }
 
-function moyennes(){
+function moyennes() {
 
     let notesPRO = stockNote.branche[0].notes,
         notesINTER = stockNote.branche[1].notes
-    
+
     // MOYENNE COMPÉTENCES EN INFORMATIQUES
 
-    let sumPRO = 0,
-        sumINTER = 0,
-        notesINFO
+    let notesINFO,
+        sumPRO = average(notesPRO),
+        sumINTER = average(notesINTER)
 
-    sumPRO = average(sumPRO, notesPRO)
-
-    sumINTER = average(sumINTER, notesINTER)
-    
-    if (sumPRO === undefined || isNaN(sumPRO) || sumPRO === 0) {
-        notesINFO = sumINTER
-    } else if(sumINTER === undefined || isNaN(sumINTER) || sumINTER === 0){
-        notesINFO = sumPRO
-    } else {
-        notesINFO = roundNumber((pond(sumPRO, 0.8))+pond(sumINTER, 0.2), 10)
-    }
-    
-
-
-    // MOYENNES COMP. DE BASE
-    
-    let sumBASE = 0,
-        notesBASE = stockNote.branche[2].notes
-
-        sumBASE = roundNumber(average(sumBASE, notesBASE),2) 
-    
-        // MOYENNE CULTURE GENERAL
-    
-    let sumCG = 0,
-        notesCG = stockNote.branche[3].notes
-
-        sumCG = roundNumber( average(sumCG, notesCG),2)
-
-    // TPI
-    let notesTPI = stockNote.branche[4].notes
+//    if (sumPRO === undefined || isNaN(sumPRO) || sumPRO === 0) {
+//        notesINFO = sumINTER
+//    } else if (sumINTER === undefined || isNaN(sumINTER) || sumINTER === 0) {
+//        notesINFO = sumPRO
+//    } else {
+//        notesINFO = roundNumber((pond(sumPRO, 0.8)) + pond(sumINTER, 0.2), 10)
+//    }
 
 
     // Put in HTML (branches moyenne)
-    
+
 
     document.getElementById("averageINFO").innerHTML = notesINFO
-    document.getElementById("averageBase").innerHTML = sumBASE
-    document.getElementById("averageCG").innerHTML = sumCG
-    document.getElementById("averageTPI").innerHTML = notesTPI
+//    document.getElementById("averageBase").innerHTML = sumBASE
+//    document.getElementById("averageCG").innerHTML = sumCG
+//    document.getElementById("averageTPI").innerHTML = notesTPI
 
     // CALCUL NOTE FINALES
 
     let noteFinales
 
-    if (noteFinales >=4) {
+    if (noteFinales >= 4) {
         document.getElementById("chiffreRE").style.background = "green"
 
         document.getElementById("textRE").style.background = "green"
@@ -178,7 +160,7 @@ function moyennes(){
     } else {
         document.getElementById("chiffreRE").style.background = "darkred"
 
-        
+
         document.getElementById("textRE").style.background = "darkred"
         document.getElementById("re").innerHTML = "Échec"
     }
@@ -188,21 +170,42 @@ function moyennes(){
 
 }
 
-function roundNumber(number, precision){
-    return Math.round(number * precision)/precision
+function roundNumber(number, precision) {
+    return Math.round(number * precision) / precision
 }
 
-function average(branche, array) {
+function average(array) {
+    let branche = 0
     for (const item of array) {
         branche += item;
     }
     return branche / array.length
 }
 
-function pond(number, pondNum){
+function pond(number, pondNum) {
     return number * pondNum
 }
 
-function ss() {
-    console.log(stockNote)
+
+function pondTEST(array) {
+    let sum = 0
+    let sumPond = 0
+    for (const i of array) {
+        sum = i[0] * i[1]
+        sumPond = i[1] + i[1]
+        console.log(sum + "  " + sumPond + "  " +array)
+    }
+    
+    return console.log(sum)
+}
+
+function ss(){
+    let aaa = [
+        [5,0.4],
+        [5,0.3],
+        [5,0.2],
+        [5,0.1]
+    ]
+
+    pondTEST(aaa)
 }
