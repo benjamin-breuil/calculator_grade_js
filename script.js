06b4c96b269c0795632a3542083f1e56a56aab00
benjaminbreuil <benjamin.breuil@jobtrek.ch>
2022-12-09 09:29:31 +0100

    FInal sans locale storage

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let stockNote = {
             name: 'ecolepro',
             notes: [
                 allSemestre = []
+            ],
+            modules: [
+                modules = []
             ]
         },
 
@@ -14,6 +17,9 @@ let stockNote = {
             name: 'coursinter',
             notes: [
                 allSemestre = []
+            ],
+            modules: [
+                modules = []
             ]
         },
 
@@ -24,21 +30,48 @@ let stockNote = {
                 semestre_math_2 = [],
                 semestre_math_3 = [],
                 semestre_anglais_1 = [],
-                semestre_anglais_1 = [],
+                semestre_anglais_2 = [],
                 semestre_anglais_3 = [],
                 semestre_anglais_4 = [],
                 semestre_anglais_5 = []
-            ]
+            ],
+
+
+            modules: [
+                branche_semestre_math_1 = [],
+                branche_semestre_math_2 = [],
+                branche_semestre_math_3 = [],
+                branche_semestre_anglais_1 = [],
+                branche_semestre_anglais_2 = [],
+                branche_semestre_anglais_3 = [],
+                branche_semestre_anglais_4 = [],
+                branche_semestre_anglais_5 = []
+            ],
+
         },
 
         {
             name: 'cultureG',
             notes: [
-                semestre1 = [],
-                semestre2 = [],
-                semestre3 = [],
-                semestre4 = [],
-                semestre5 = []
+                semestre_cultureG_1 = [],
+                semestre_cultureG_2 = [],
+                semestre_cultureG_3 = [],
+                semestre_cultureG_4 = [],
+                semestre_cultureG_5 = [],
+                semestre_cultureG_6 = [],
+                semestre_cultureG_7 = [],
+                semestre_cultureG_8 = []
+            ],
+
+            modules: [
+                branche_semestre_cultureG_1 = [],
+                branche_semestre_cultureG_2 = [],
+                branche_semestre_cultureG_3 = [],
+                branche_semestre_cultureG_4 = [],
+                branche_semestre_cultureG_5 = [],
+                branche_semestre_cultureG_6 = [],
+                branche_semestre_cultureG_7 = [],
+                branche_semestre_cultureG_8 = [],
             ]
         },
 
@@ -46,6 +79,9 @@ let stockNote = {
             name: 'tpi',
             notes: [
                 allSemestre = []
+            ],
+            modules: [
+                modules = []
             ]
         }
     ]
@@ -54,51 +90,61 @@ let stockNote = {
 window.addEventListener('load', () => {
     eventSemestre()
     choice = -1
-    console.log(choice)
 })
 
+
+moyennes()
+
 function eventSemestre() {
-    document.getElementById("sem3").value = "-1"
+    document.getElementById("modules3").value = "-1"
     document.getElementById("modules2").value = "-1"
 }
 
+for (var i = 0; i <= 4; i++) {
+    allFunction(i);
+}
 
-chooseIndex(3)
-chooseIndex(2)
-
-
-// ADD ROW
-addRow(0), addRow(1), addRow(2), addRow(3), addRow(4)
-// EDIT ROW
-editRow(0), editRow(1), editRow(2), editRow(3), editRow(4)
-// REM. ROW
-removeRow(0), removeRow(1), removeRow(2), removeRow(3), removeRow(4)
-//
+function allFunction(index) {
+    if (index === 0) {
+        addRow(0), editRow(0), removeRow(0)
+    } if (index === 1) {
+        addRow(1), editRow(1), removeRow(1)
+    } if (index === 2) {
+        addRow(2), editRow(2), removeRow(2), chooseIndex(2), deleteTable(2)
+    } if (index === 3) {
+        addRow(3), editRow(3), removeRow(3), chooseIndex(3), deleteTable(3)
+    } if (index === 4) {
+        addRow(4), editRow(4), removeRow(4)
+    }
+}
 
 function addNoteInObject(id) {
     let note = document.getElementById("notes" + id).valueAsNumber
+    let modules = document.getElementById("modules" + id).value
+
     chooseIndex(id)
 
     if (!isNaN(note)) {
         if (id === 0 || id === 1 || id === 4) {
             stockNote.branche[id].notes[0].push(note)
+            stockNote.branche[id].modules[0].push(modules)
+            //            localStorage.setItem('notes', stockNote.branche[id].notes[0])
         } else {
             stockNote.branche[id].notes[choice].push(note)
+            stockNote.branche[id].modules[choice].push(modules)
+
+            //            localStorage.setItem('notes', stockNote.branche[id].notes[choice])
         }
     } else {
         return
     }
-
-
-
-    console.log(stockNote)
 }
 
 function addRow(id) {
     const button = document.getElementById('button' + id)
 
     button.addEventListener('click', function () {
-        let table = document.getElementById("table" + id),
+        let table = document.getElementById("table-" + id),
             notes = document.getElementById("notes" + id).valueAsNumber
         if (id === 0 && !isNaN(notes) || id === 1 && !isNaN(notes) || id === 4 && !isNaN(notes)) {
             modules = document.getElementById("modules" + id).value
@@ -119,18 +165,17 @@ function addRow(id) {
                     let newRow = table.insertRow(table.length),
                         cell1 = newRow.insertCell(0),
                         cell2 = newRow.insertCell(1),
-                        modules = "Math"
+                        modules = document.getElementById('modules' + id).value
                     cell1.innerHTML = modules
                     cell2.innerHTML = notes
                 } else {
                     let newRow = table.insertRow(table.length),
-                    cell1 = newRow.insertCell(0),
-                    cell2 = newRow.insertCell(1),
-                    modules = "Anglais"
-                cell1.innerHTML = modules
-                cell2.innerHTML = notes
+                        cell1 = newRow.insertCell(0),
+                        cell2 = newRow.insertCell(1),
+                        modules = document.getElementById('modules' + id).value
+                    cell1.innerHTML = modules
+                    cell2.innerHTML = notes
                 }
-
             }
         } else if (id === 2 || id === 3) {
             alert("Choisis un semestre valide")
@@ -149,7 +194,7 @@ function addRow(id) {
 function editRow(id) {
     const button = document.getElementById('buttonEdit' + id)
     button.addEventListener('click', function () {
-        let table = document.getElementById("table" + id),
+        let table = document.getElementById("table-" + id),
             modules = document.getElementById("modules" + id).value,
             notes = document.getElementById("notes" + id).valueAsNumber
         randomIndexFunction(table, id)
@@ -159,7 +204,7 @@ function editRow(id) {
         if (id === 0 || id === 1 || id === 4) {
             stockNote.branche[id].notes[0][rIndex - 1] = notes
         } else {
-            stockNote.branche[id].notes[choice][rIndex - 1] = notes
+            stockNote.branche[id].notes[choice][rIndex] = notes
         }
         //--------------------------------------------------------------
 
@@ -174,7 +219,7 @@ function editRow(id) {
 function removeRow(id) {
     const button = document.getElementById('buttonREM' + id)
     button.addEventListener('click', function () {
-        let table = document.getElementById("table" + id)
+        let table = document.getElementById("table-" + id)
 
         randomIndexFunction(table, id)
 
@@ -183,106 +228,35 @@ function removeRow(id) {
 
         if (id === 0 || id === 1 || id === 4) {
             stockNote.branche[id].notes[0].splice(rIndex - 1, 1)
+            stockNote.branche[id].modules[0].splice(rIndex - 1, 1)
+
         } else {
             stockNote.branche[id].notes[choice].splice(rIndex - 1, 1)
+            stockNote.branche[id].modules[choice].splice(rIndex - 1, 1)
+
         }
-
-
         // -------------------------------------------------
 
         table.deleteRow(rIndex)
-        document.getElementById("notes" + id).value = "";
-        document.getElementById("modules" + id).value = "";
         moyennes()
     })
 
 }
 
 function randomIndexFunction(tables, ids) {
-    for (var i = 1; i < tables.rows.length; i++) {
+    for (var i = 0; i < tables.rows.length; i++) {
         tables.rows[i].onclick = function () {
             rIndex = this.rowIndex
             document.getElementById("modules" + ids).value = this.cells[0].innerHTML
             document.getElementById("notes" + ids).value = this.cells[1].innerHTML
             console.log("curreeent row " + rIndex)
-            console.log(rIndex)
             Array.from(this.parentElement.children).forEach(function (el) {
                 el.classList.remove('selected-row')
             })
             this.classList.add('selected-row')
         }
     }
-
     return rIndex
-}
-
-function moyennes() {
-    // MOYENNE COMPÉTENCES EN INFORMATIQUES
-
-    let notesPRO = stockNote.branche[0].notes[0],
-        notesINTER = stockNote.branche[1].notes[0]
-
-    console.log('JSUISS DNASNAIFHASF' + "   " + notesPRO)
-
-
-    let notesINFO,
-        sumPRO = roundNumber(average(notesPRO), 10),
-        sumINTER = roundNumber(average(notesINTER), 10)
-
-    if (sumPRO === undefined || isNaN(sumPRO) || sumPRO === 0) {
-        notesINFO = sumINTER
-    } else if (sumINTER === undefined || isNaN(sumINTER) || sumINTER === 0) {
-        notesINFO = sumPRO
-    } else {
-        notesINFO = roundNumber(pond(sumPRO, 0.8), 10) + roundNumber(pond(sumINTER, 0.2), 10)
-    }
-
-    // Moyennes par semestre math anglais
-
-    // Put the note in color green black or red depending of the notes
-
-
-    redBlackGreen(notesINFO, "averageINFO")
-
-
-
-
-    // CALCUL NOTE FINALES
-
-    let noteFinales
-
-    if (noteFinales >= 4) {
-        document.getElementById("chiffreRE").style.background = "green"
-
-        document.getElementById("textRE").style.background = "green"
-        document.getElementById("re").innerHTML = "Réussie"
-    } else {
-        document.getElementById("chiffreRE").style.background = "darkred"
-
-
-        document.getElementById("textRE").style.background = "darkred"
-        document.getElementById("re").innerHTML = "Échec"
-    }
-
-    document.getElementById("cfc").innerHTML = noteFinales
-
-
-}
-
-function roundNumber(number, precision) {
-    return Math.round(number * precision) / precision
-}
-
-function average(array) {
-    let branche = 0
-    for (const item of array) {
-        branche += item;
-    }
-    return branche / array.length
-}
-
-function pond(number, pondNum) {
-    return number * pondNum
 }
 
 function redBlackGreen(notes, id) {
@@ -299,19 +273,18 @@ function redBlackGreen(notes, id) {
     }
 }
 
-
 function chooseIndex(id) {
     id
     if (id = 2) {
         selected = document.getElementsByTagName('select')[0].onchange = function () {
             choice = this.selectedIndex - 1;
-            console.log(choice);
+            console.log("choice " + choice);
             return choice
         }
     } if (id = 3) {
         selected = document.getElementsByTagName('select')[1].onchange = function () {
             choice = this.selectedIndex - 1;
-            console.log(choice);
+            console.log("choice " + choice);
             return choice
         }
     }
@@ -319,23 +292,42 @@ function chooseIndex(id) {
     return choice
 }
 
+function deleteTable(index) {
+    let selector = document.querySelectorAll(".select");
+    for (const iterator of selector) {
+        iterator.addEventListener("change", function (e) {
+            //            let index = e.target.id.split("-")[1]
+            chooseIndex(index)
+            let table = document.getElementById("table-" + index)
+            let rowLength = table.rows.length;
+            for (var i = 1; i <= rowLength; i++) {
+                table.deleteRow(0);
+            }
+            //Adds the stored notes in table
+            //            let semestre = e.target.value;
+            let rightNote = stockNote.branche[index].notes[choice]
 
-// TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
-function pondTEST(array) {
-    let sum = 0
-    let sumPond = 0
-    for (const i of array) {
-        sum = i[0] * i[1]
-        sumPond = sumPond += i[1]
-        console.log("sum normal " + sum)
-        console.log("sum pondération " + sumPond)
+            let branches = stockNote.branche[index].modules[choice][0]
+
+
+            for (const grade of rightNote) {
+                let row = table.insertRow(0)
+                let cell1 = row.insertCell(0)
+                let cell2 = row.insertCell(1)
+                randomIndexFunction(table, index)
+                cell2.innerHTML = grade
+                cell1.innerHTML = branches
+            }
+
+
+        });
     }
-
-    return sum
 }
 
-function ss() {
+// TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
 
+
+function ss() {
     console.log(stockNote)
 }
 
@@ -343,4 +335,161 @@ function ss() {
 
 // Notes : J'aurai du commencé par lister toute les fonctionnalités ect mal organisé
 
+// ------MOYENNES-------------------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------MOYENNES--------------------------------
+// ---------------------MOYENNES----------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------------MOYENNES---
 
+function moyennes() {
+    // MOYENNE COMPÉTENCES EN INFORMATIQUES
+
+    let notesINFO = noteFinal(0, 1, 0, 0, 5, 0.8, 0.2, 1)
+
+    redBlackGreen(notesINFO, "averageINFO")
+
+    // Compétence de base élargie
+
+    let semestreB1 = noteFinal(2, 2, 0, 3, 2, 1, 1, 2)
+    let semestreB2 = noteFinal(2, 2, 1, 4, 2, 1, 1, 2)
+    let semestreB3 = noteFinal(2, 2, 2, 5, 2, 1, 1, 2)
+    let semestreB4 = roundNumber(average(stockNote.branche[2].notes[6]), 2)
+    let semestreB5 = roundNumber(average(stockNote.branche[2].notes[7]), 2)
+
+
+    let notesBASE = [semestreB1, semestreB2, semestreB3, semestreB4, semestreB5];
+    let finalGrade = roundNumber(calculateFinalGrade(notesBASE),2) ;
+
+
+
+
+    redBlackGreen(finalGrade, "averageBASE")
+
+
+    console.log("notes finales base élargie " + finalGrade)
+
+        // Moyenne culture G 
+
+        let semestreG1 = roundNumber(average(stockNote.branche[3].notes[0]),2)
+        let semestreG2 = roundNumber(average(stockNote.branche[3].notes[1]),2) 
+        let semestreG3 = roundNumber(average(stockNote.branche[3].notes[2]),2) 
+        let semestreG4 = roundNumber(average(stockNote.branche[3].notes[3]),2)
+        let semestreG5 = roundNumber(average(stockNote.branche[3].notes[4]),2)  
+        let semestreG6 = roundNumber(average(stockNote.branche[3].notes[5]),2) 
+        let semestreG7 = roundNumber(average(stockNote.branche[3].notes[6]),2) 
+        let semestreG8 = roundNumber(average(stockNote.branche[3].notes[7]),2) 
+
+
+        var notesG = [semestreG1, semestreG2, semestreG3, semestreG4, semestreG5, semestreG6, semestreG7, semestreG8];
+        var finalGradeCG = roundNumber(calculateFinalGrade(notesG),2) ;
+
+        redBlackGreen(finalGradeCG, "averageCG")
+        console.log(semestreG1 + " raaaaaat")
+
+
+        // TPI NOTE
+
+        let noteTPI = stockNote.branche[4].notes[0]
+
+        redBlackGreen(noteTPI, "averageTPI")
+    // CALCUL NOTE FINALES
+
+    let allNote = roundNumber((pond(finalGradeCG, 0.2) + pond(finalGrade, 0.1) + pond(notesINFO, 0.3) + pond(noteTPI,0.4)),10)
+    let noteFinale = allNote
+
+    if (noteFinale >= 4) {
+        document.getElementById("chiffreRE").style.background = "green"
+
+        document.getElementById("textRE").style.background = "green"
+        document.getElementById("re").innerHTML = "Réussie"
+    } else {
+        document.getElementById("chiffreRE").style.background = "darkred"
+
+
+        document.getElementById("textRE").style.background = "darkred"
+        document.getElementById("re").innerHTML = "Échec"
+    }
+
+    document.getElementById("cfc").innerHTML = noteFinale
+
+
+}
+
+function pond(number, pondNum) {
+    return number * pondNum
+}
+
+function roundNumber(number, precision) {
+
+    if (isNaN(number)) {
+        number = null
+        return number
+    } else {
+        return Math.round(number * precision) / precision
+    }
+
+
+}
+
+function average(array) {
+    let branche = 0
+    for (const item of array) {
+        branche += item;
+    }
+    return branche / array.length
+}
+
+function noteFinal(branche1, branche2, note1, note2, averages, pond1, pond2, division) {
+    let notesFinal,
+        sum1 = roundNumber(average(stockNote.branche[branche1].notes[note1]), averages),
+        sum2 = roundNumber(average(stockNote.branche[branche2].notes[note2]), averages)
+
+
+
+    if (sum2 === undefined || isNaN(sum2) || sum2 === 0 || sum2 === null) {
+        notesFinal = sum1
+    } else if (sum1 === undefined || isNaN(sum1) || sum1 === 0 || sum1 === null) {
+        notesFinal = sum2
+    } else {
+        notesFinal = (roundNumber(pond(sum1, pond1), averages) + roundNumber(pond(sum2, pond2), averages)) / division
+    }
+
+    if (isNaN(notesFinal) || notesFinal === 0) {
+        notesFinal = null
+
+    }
+
+    return notesFinal
+}
+
+
+function finleee(array) {
+    let branche = 0
+    for (const item of array) {
+        branche += item;
+    }
+
+    return branche 
+}
+
+
+
+function calculateFinalGrade(grades) {
+    // Assigner la valeur zéro à la note finale
+    let finalGrade = 0;
+    
+    // Compter le nombre de notes non nulles et non non définies
+    let count = 0;
+    
+    // Parcourir chaque note dans le tableau de notes
+    for (var i = 0; i < grades.length; i++) {
+      // Si la note est non nulle et non définie, ajouter la note à la note finale et incrémenter le compteur
+      if (grades[i] != 0 && grades[i] != undefined && grades[i] != null) {
+        finalGrade += grades[i];
+        count++;
+      }
+    }
+    
+    // Si le compteur est supérieur à zéro, calculer la moyenne des notes non nulles et non définies et retourner la valeur
+    // Sinon, retourner zéro
+    return count > 0 ? finalGrade / count : 0;
+  }
--- test.js
+++ test.js
@@ -8,18 +8,18 @@ myChoice.onchange = (event) => {
 
 
 function addNoteInObject(id) {
-let note = document.getElementById("notes" + id).valueAsNumber
-var myChoice = document.getElementById('semestre-c-de-base');
-myChoice.onchange = (event) => {
-    var choice = event.target.value;
-    if (id === 0 || id === 1 || id === 4) {
-        stockNote.branche[id].notes[0].push(note)
-    } else {
-        stockNote.branche[id].notes[choice].push(note)
+    let note = document.getElementById("notes" + id).valueAsNumber
+    var myChoice = document.getElementById('semestre-c-de-base');
+    myChoice.onchange = (event) => {
+        var choice = event.target.value;
+        if (id === 0 || id === 1 || id === 4) {
+            stockNote.branche[id].notes[0].push(note)
+        } else {
+            stockNote.branche[id].notes[choice].push(note)
+        }
+        console.log(choice);
     }
-    console.log(choice);
-}
-console.log(stockNote)
+    console.log(stockNote)
 }
 
 
@@ -27,41 +27,41 @@ console.log(stockNote)
 
 
 if (id === 0 || id === 1 || id === 4) {
-stockNote.branche[id].notes[0][rIndex - 1] = notes
+    stockNote.branche[id].notes[0][rIndex - 1] = notes
 } else {
-stockNote.branche[id].notes[choice][rIndex - 1] = notes
+    stockNote.branche[id].notes[choice][rIndex - 1] = notes
 }
 
 
 
 
 if (id === 0 || id === 1 || id === 4) {
-stockNote.branche[id].notes[0].splice(rIndex - 1, 1)
+    stockNote.branche[id].notes[0].splice(rIndex - 1, 1)
 } else {
-stockNote.branche[id].notes[choice].splice(rIndex - 1, 1)
+    stockNote.branche[id].notes[choice].splice(rIndex - 1, 1)
 }
 
 
 
 
 function name(x, y) {
-document.getElementsByTagName('select')[y].onchange = function() {
-    let choice = this.selectedIndex;
-    if (id === 0 || id === 1 || id === 4) {
-        stockNote.branche[id].notes[0].push(note)
-    } else if (id === x){
-        stockNote.branche[id].notes[choice].push(note)
+    document.getElementsByTagName('select')[y].onchange = function () {
+        let choice = this.selectedIndex;
+        if (id === 0 || id === 1 || id === 4) {
+            stockNote.branche[id].notes[0].push(note)
+        } else if (id === x) {
+            stockNote.branche[id].notes[choice].push(note)
+        }
+        console.log(choice);
     }
+}
+
+
+
+
+document.getElementsByTagName('select')[0].onchange = function () {
+    var choice = this.selectedIndex;
     console.log(choice);
-}
-}
-
-
-
-
-document.getElementsByTagName('select')[0].onchange = function() {
-var choice = this.selectedIndex;
-console.log(choice);
 }
 
 
@@ -77,8 +77,8 @@ cell1.innerHTML = grade;
 
 
 if (!(id === 2 || id === 3)) {
-} else if (id === 2){
-} else if (id === 3){
+} else if (id === 2) {
+} else if (id === 3) {
 }
 
 
@@ -87,26 +87,26 @@ if (!(id === 2 || id === 3)) {
 if (!(id === 2 || id === 3)) {
     randomIndexFunction(table, id)
     let table = document.getElementById("table" + id),
-    modules = document.getElementById("modules" + id).value,
-    notes = document.getElementById("notes" + id).valueAsNumber
+        modules = document.getElementById("modules" + id).value,
+        notes = document.getElementById("notes" + id).valueAsNumber
     table.rows[rIndex].cells[0].innerHTML = modules;
     table.rows[rIndex].cells[1].innerHTML = notes;
-} else if (id === 2){
+} else if (id === 2) {
     chooseIndex(id)
     console.log(choice)
     let semestre = document.getElementById("semestre" + choice)
     randomIndexFunction(semestre, choice)
     let modules = document.getElementById("modules" + id).value,
-    notes = document.getElementById("notes" + id).valueAsNumber
+        notes = document.getElementById("notes" + id).valueAsNumber
     semestre.rows[rIndex].cells[0].innerHTML = modules;
     semestre.rows[rIndex].cells[1].innerHTML = notes;
 
-} else if (id === 3){
+} else if (id === 3) {
     chooseIndex(id)
     let semestre = document.getElementById("semestreG" + choice)
     randomIndexFunction(semestre, choice)
     let modules = document.getElementById("modules" + id).value,
-    notes = document.getElementById("notes" + id).valueAsNumber
+        notes = document.getElementById("notes" + id).valueAsNumber
     semestre.rows[rIndex].cells[0].innerHTML = modules;
     semestre.rows[rIndex].cells[1].innerHTML = notes;
 
@@ -129,13 +129,13 @@ if (id === 2 && choice != -1 || id === 3 && choice != -1) {
                 cell2.innerHTML = notes
             } else {
                 let newRow = table.insertRow(table.length),
-                cell1 = newRow.insertCell(0),
-                cell2 = newRow.insertCell(1),
-                modules = "Anglais"
-            cell1.innerHTML = modules
-            cell2.innerHTML = notes
+                    cell1 = newRow.insertCell(0),
+                    cell2 = newRow.insertCell(1),
+                    modules = "Anglais"
+                cell1.innerHTML = modules
+                cell2.innerHTML = notes
             }
-        } else  {
+        } else {
             modules = document.getElementById("modules" + id).value
             let newRow = table.insertRow(table.length),
                 cell1 = newRow.insertCell(0),
@@ -156,6 +156,130 @@ function eventSemestre() {
 
 
 
+function changeTable(selectedID, id) {
+    var select = document.getElementById("selectNumber");
+    var options = ["ratio", "blaireau", "3", "4", "5"];
+
+    for (var i = 0; i < options.length; i++) {
+        var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
+    }
+}
 
 
-// Filtre depends des semestre
+
+
+function changeTable(id) {
+    if (id === 0 || id === 1 || id === 4) {
+        let options = stockNote.branche[id].notes[0],
+            newRow = table.insertRow(table.length),
+            cell1 = newRow.insertCell(0)
+        for (var i = 0; i < options.length; i++) {
+            cell1.innerHTML = stockNote.branche[id].notes[0]
+        }
+    } else {
+        let options = stockNote.branche[id].notes[choice],
+            newRow = table.insertRow(table.length),
+            cell1 = newRow.insertCell(0)
+        for (var i = 0; i < options.length; i++) {
+            cell1.innerHTML = stockNote.branche[id].notes[choice]
+        }
+    }
+}
+
+
+
+function deleteTable() {
+    let selector = document.querySelectorAll(".select");
+    for (const iterator of selector) {
+        iterator.addEventListener("change", function (e) {
+            let index = e.target.id.split("-")[1];
+            let table = document.getElementById("table" + index);
+            let rowLength = table.rows.length;
+            for (var i = 1; i <= rowLength; i++) {
+                table.deleteRow(0);
+            }
+            //Adds the stored notes in table
+            let semestre = e.target.value;
+            let rightNote = Notes["table" + index][semestre]
+            for (const grade of rightNote) {
+                let row = table.insertRow(0);
+                let cell1 = row.insertCell(0);
+                cell1.innerHTML = grade;
+            }
+        });
+    }
+}
+
+deleteTable() 
+
+function deleteTable(index) {
+    let selector = document.querySelectorAll(".select");
+    chooseIndex(index)
+    for (const iterator of selector) {
+        iterator.addEventListener("change", function (e) {
+        //    let index = e.target.id.split("-")[1];
+            let table = document.getElementById("table-" + index);
+            var rowLength = table.rows.length;
+            for (var i = rowLength - 1; i > 1; i--) {
+                table.deleteRow(i);
+            }
+            //Adds the stored notes in table
+//            let semestre = e.target.value;
+            let rightNote = stockNote.branche[index].notes[choice]
+            for (const grade of rightNote) {
+                let row = table.insertRow(0);
+                let cell1 = row.insertCell(0);
+                cell1.innerHTML = grade;
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function elseIfElseIfElse(un,deux,trois,quatre,cinq,six) {
+    if (un === undefined || isNaN(un) || un === 0) {
+        baseNote = roundNumber(((deux + trois + quatre + cinq) / 4), 2)
+    } else if (un === undefined || isNaN(un) || un === 0) {
+        baseNote = roundNumber(((trois + quatre + cinq) / 3), 2)
+    } else if (un === undefined || isNaN(un) || un === 0){
+        baseNote = roundNumber(((quatre + cinq) / 2), 2)
+    } else if (un === undefined || isNaN(un) || un === 0){
+        baseNote = roundNumber(((cinq) / 1), 2)
+    } else {
+        baseNote = six
+    }
+}
+
+function nameF(un, deux, trois, quatre, cinq) {
+
+    if (un === undefined || isNaN(un) || un === 0) {
+        baseNote = roundNumber(((deux + trois + quatre + cinq) / 4), 2)
+    } else if (un === undefined || isNaN(un) || un === 0 && deux === undefined || isNaN(deux) || deux === 0) {
+        baseNote = roundNumber(((trois + quatre + cinq) / 3), 2)
+    }
+
+    return baseNote
+}
+
+function nameF(un, deux, trois, quatre, cinq) {
+
+    if (un === undefined || isNaN(un) || un === 0) {
+        baseNote = roundNumber(((deux + trois + quatre + cinq) / 4), 2)
+    } 
+
+    return baseNote
+}
